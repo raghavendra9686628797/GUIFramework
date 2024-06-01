cd290037ca2872ecd9b5b1cc5c28fa2066eecaae
Sanjay-Neelagund <sanjay.g01@fireflink.com>
2024-06-01 13:11:21 +0530

    Added object page & pom class

---
+++ GUISeleniumFramework/src/main/java/VendorsPage.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class VendorsPage {
+	WebDriver driver;
+	public VendorsPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(xpath = "//img[@title=\"Create Vendor...\"]")
+	private WebElement createLeadImg;
+	
+	@FindBy(name="search_text")
+	private WebElement searchForedt;
+	
+	@FindBy(name="bas_searchfield")
+	private WebElement searchFieldDD;
+	
+	@FindBy(xpath="//input[contains(@value,\"Search Now\")]")
+	private WebElement searchNowBtn;
+
+}
