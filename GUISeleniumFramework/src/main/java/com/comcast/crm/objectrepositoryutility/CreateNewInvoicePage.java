83d049bafd79802b529c817dfdb8e61f3fcd191f
Ashaacchu <ashartnp1@gmail.com>
2024-06-03 15:55:45 +0530

    CreateNewInvoice page and some elements is added

---
+++ GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/CreateNewInvoicePage.java
@@ -0,0 +1,22 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CreateNewInvoicePage {
+	WebDriver driver;
+	public CreateNewInvoicePage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(xpath = "//span[text()='Creating New Invoice']")private WebElement createNewInvoiceTxt;
+	@FindBy(xpath = "//input[@name=\"subject\"]") private WebElement subjectTxtBox;
+	@FindBy(xpath = "//input[@name=\"account_name\"]/following-sibling::img")private WebElement selectorgBtn;
+	@FindBy(xpath = "//textarea[@name=\"bill_street\"]") private WebElement billingAddTextArea;
+	@FindBy(xpath = "//input[@onclick=\"return copyAddressRight(EditView)\"]") private WebElement copyBilAddRadioBtn;
+	@FindBy(xpath = "//input[@title=\"Save [Alt+S]\"]") private WebElement saveBtn;
+	@FindBy(id = "searchIcon1") private WebElement selectItemBtn;
+	@FindBy(id="qty1") private WebElement qtyTxtBox;
+}
