c5e3f74f67dec7b6ce9ede0979e3d052bb73babd
Ashaacchu <ashartnp1@gmail.com>
2024-06-03 12:34:23 +0530

    Added opportunity info page and element

---
+++ GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/OpportunityInformationPage.java
@@ -0,0 +1,17 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OpportunityInformationPage {
+	WebDriver driver;
+	public OpportunityInformationPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(xpath = "//span[@class=\"dvHeaderText\"]")
+	private WebElement headerTxt;
+	
+}
--- GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/OpportunityPage.java
+++ GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/OpportunityPage.java
@@ -1,5 +1,16 @@
 package com.comcast.crm.objectrepositoryutility;
 
-public class OpportunityPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
+public class OpportunityPage {
+	WebDriver driver;
+	public OpportunityPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+	
+	
 }
